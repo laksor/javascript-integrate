69918c55411db681a470abb4db9bc22c0e1bcec2
laskor <al.moksud@gmail.com>
2022-02-12 22:50:57 -0500

    comment post code

--- script.js
+++ script.js
@@ -33,3 +33,39 @@ const hotPink = document.getElementById('hot-pink');
 document.getElementById('khaki').addEventListener('click', function (){
     document.body.style.backgroundColor = 'khaki';
 })
+
+// common ways to set event handlers
+
+function handelJustFunctionClick(){
+    const p = document.getElementById("clickingMethod");
+    p.innerText = 'set by using function';
+}
+
+// 2nd way
+
+document.getElementById("handel-event").addEventListener('click', function (){
+    const p = document.getElementById("clickingMethod");
+    p.innerText = 'set by event listener';
+})
+
+// input.value
+
+document.getElementById("update-name").addEventListener('click', function(){
+    const nameField = document.getElementById("name-field");
+    const p = document.getElementById("nama");
+    p.innerText = nameField.value;
+    nameField.value = '';
+})
+
+// comment post
+
+document.getElementById('submit-comment').addEventListener('click', function (){
+    const commentBox = document.getElementById('text-area');
+    const newComment = document.createElement('p');
+    newComment.innerText = commentBox.value;
+    
+    const commentArea = document.getElementById('comment-area');
+    commentArea.appendChild(newComment);
+
+    commentBox.value = '';
+})
